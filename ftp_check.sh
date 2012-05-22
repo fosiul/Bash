63149b83986e0cb740ba32b31a72eb652f4c8db4
Fosiul Alam <fosiul@gmail.com>
2012-05-22 12:43:21 +0100

    Variables added

--- ftp_check.sh
+++ ftp_check.sh
@@ -2,15 +2,15 @@
  
 #Version 1.1
 #######################################
-#Nagios script to check ftp server##
+#Nagios script to check ftp server status##
 #Commands : wget --quiet --spider --tries=1 -P /tmp ftp://user:password@ftp.ftp-server.co.uk 
-#Status check for nagios script
+
  
 STATE_OK=0
 STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
-STATE_DEPENDENT=4
+
 
  
 #Define All the variables for commands
@@ -18,20 +18,39 @@ STATE_DEPENDENT=4
 declare -rx SCRIPT=${0##*/}
 declare  -rx CMD_WGET="/usr/bin/wget"
 
-USER_NAME=$1
-USER_PASS=$2
-FTP_ADDR=$3
 
-	$CMD_WGET --quiet --spider --tries=1 -P /tmp ftp://$USER_NAME:$USER_PASS@$FTP_ADDR
-		result=$?
+MINPARAMS=4 # This script needs 3 parameter
+USER_NAME=$1 # ftp username
+USER_PASS=$2 # ftp password
+FTP_ADDR=$3   # ftp address
+WGET_DIR=$4   # Define a directory where "wget" will dump files.
+              #Nagios script should have writable permission on this directory, 
+              #other wise wget will return error code 3
+
+SUCCESS=0  # 
+
+#Find out if user has provided 3 parameters .
+
+
+if [ $# -lt "$MINPARAMS" ]
+then 
+	echo "This scripts needs at least $MINPARAMS command-line arguments!"
+	exit $STATE_UNKNOWN
+fi
+
+
+
+	$CMD_WGET --quiet --spider --tries=1 -P "$4" ftp://$USER_NAME:$USER_PASS@$FTP_ADDR
+		
 	
-			if [ $result != 0  ] 
+			if [ $? -ne  $SUCCESS  ] 
 			then
-					echo "ftp server is offline:"$result
+					echo "ftp server $FTP_ADDR is offline"
 					exit $STATE_CRITICAL
 			else
-				echo "ftp server is online: "$result
+				echo "ftp server $FTP_ADDR is online "
 				exit $STATE_OK
 		fi
-		
+
+
 		
